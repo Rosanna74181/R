cb2863526d21a8db0ed04aee19bbea2e28078899
Rosanna <rosanna74181@gmail.com>
2024-09-11 10:22:54 +0800

    修改

--- src/app/TestApp.java
+++ src/app/TestApp.java
@@ -1,5 +1,9 @@
 package app;
 
-public class TestApp {
+import java.awt.print.Printable;
 
+public class TestApp {
+	public static void main(String[] args) {
+		System.out.println("歡迎報名夜間JAVA班!!");
+	}
 }
